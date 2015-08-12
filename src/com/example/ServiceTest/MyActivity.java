e492ce2af98725f6de801b04244d3a026d68267e
chinadarren <chinadarren@outlook.com>
2015-08-13 01:48:57 +0800

    Initial commit

---
+++ src/com/example/ServiceTest/MyActivity.java
@@ -0,0 +1,75 @@
+package com.example.ServiceTest;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.view.View;
+import android.widget.Button;
+
+public class MyActivity extends Activity implements View.OnClickListener {
+    /**
+     * Called when the activity is first created.
+     */
+    private Button startService;
+    private Button stopService;
+    private Button bindService;
+    private Button unbindService;
+
+    private MySerivce.DownloadBinder downloadBinder;
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            downloadBinder = (MySerivce.DownloadBinder) service;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        startService = (Button) findViewById(R.id.start_service);
+        stopService = (Button) findViewById(R.id.stop_service);
+        stopService.setOnClickListener(this);
+        startService.setOnClickListener(this);
+
+        bindService = (Button) findViewById(R.id.bind_service);
+        unbindService = (Button) findViewById(R.id.unbind_service);
+        bindService.setOnClickListener(this);
+        unbindService.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.start_service:
+                Intent startIntent = new Intent(this,MySerivce.class);
+                startService(startIntent);//启动服务
+                break;
+            case R.id.stop_service:
+                Intent stopIntent = new Intent(this,MySerivce.class);
+                stopService(stopIntent);//停止服务
+                break;
+            case R.id.bind_service:
+                Intent bindIntent = new Intent(this,MySerivce.class);
+                //BIND_AUTO_CREATE 表示在活动和服务进行绑定后自动创建服务
+                bindService(bindIntent,connection,BIND_AUTO_CREATE);//绑定服务
+                break;
+            case R.id.unbind_service:
+                unbindService(connection);//解绑服务
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ src/com/example/ServiceTest/MySerivce.java
@@ -0,0 +1,46 @@
+package com.example.ServiceTest;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+/**
+ * Created by cl on 2015/8/13.
+ */
+public class MySerivce extends Service {
+    private DownloadBinder mBinder = new DownloadBinder();
+    class DownloadBinder extends Binder {
+        public void startDownload(){
+            Log.d("MyService","startDownload executed");
+        }
+        public int getProgress(){
+            Log.d("MyService","getProgress executed");
+            return 0;
+        }
+
+    }
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        Log.d("MyService","onCreate executed");
+    }
+    @Override
+    public int onStartCommand(Intent intent,int flags,int startId){
+        Log.d("MyService","onStartCommand executed");
+
+       return super.onStartCommand(intent, flags, startId);
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d("MyService", "onD executed");
+
+    }
+
+}
